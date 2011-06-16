93ecd0967b7dd9197cca294647c1cd477db9aa80
Aphrodite.Spartacus <Aphrodite@SPARTACUS.(none)>
2011-06-16 17:34:10 +0800

    fighting

---
+++ spoj/CANDY3/1.cpp
@@ -0,0 +1,53 @@
+#include <cctype>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <algorithm>
+#include <bitset>
+#include <deque>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+using namespace std;
+
+#ifdef DEBUG
+#include "/home/Aphrodite/program/acm/topcoder/libs/debug.cpp"
+#endif
+
+/*******************************************************************************
+ *                                                                             *
+ *                  To see the world in a grain of sand,                       *
+ *                  And a heaven in a wild flower;                             *
+ *                  Hold infinity in the palm of your hand,                    *
+ *                  And eternity in an hour.                                   *
+ *                                                                             *
+ ******************************************************************************/
+
+
+int main()
+{
+    int T;
+    scanf("%d", &T);
+    for (int ti = 0; ti < T; ++ti) {
+        long long n, x, cnt = 0;
+        scanf("%lld", &n);
+        for (long long i = 0; i < n; ++i) {
+            scanf("%lld", &x);
+            cnt = (cnt + x) % n;
+        }
+        if (!cnt) puts("YES");
+        else puts("NO");
+    }
+}
