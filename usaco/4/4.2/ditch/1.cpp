3b8660eba6bee904d9dfccdc5382fdb1d6e5a553
Aphrodite <rock.spartacus@gmail.com>
2011-11-07 14:56:04 +0800

    fighting

--- usaco/4/4.2/ditch/1.cpp
+++ usaco/4/4.2/ditch/1.cpp
@@ -1,7 +1,7 @@
 /*
   ID:   aphrodi1
   LANG: C++
-  PROG: replaceme
+  PROG: ditch
 */
 
 #include <cctype>
@@ -32,8 +32,8 @@ using namespace std;
 #include "/Users/Aphrodite/program/acm/lib/debug.cpp"
 #endif
 
-ifstream fin("replaceme.in");
-ofstream fout("replaceme.out");
+ifstream fin("ditch.in");
+ofstream fout("ditch.out");
 streambuf *cin_buf = cin.rdbuf();
 streambuf *cout_buf = cout.rdbuf();
 
@@ -46,6 +46,46 @@ streambuf *cout_buf = cout.rdbuf();
  *                                                                          *
  ***************************************************************************/
 
+int findAugmentingPath(vector<vector<int> > &g, int source, int dest)
+{
+    int n = (int)(g).size();
+    vector<int> from(n, -1);
+    queue<int> q;
+    q.push(source);
+    while (!q.empty()) {
+	int cur = q.front();
+	q.pop();
+	if (cur == dest) break;
+	for (int next = 0; next < n; ++next)
+	    if (next != source && from[next] == -1 && g[cur][next] > 0) {
+                from[next] = cur;
+                q.push(next);
+            }
+    }
+    int cur = dest;
+    int res = INT_MAX;
+    while (from[cur] != -1) {
+        res = min(res, g[from[cur]][cur]);
+        cur = from[cur];
+    }
+    cur = dest;
+    while (from[cur] != -1) {
+        g[from[cur]][cur] -= res;
+        g[cur][from[cur]] += res;
+        cur = from[cur];
+    }
+    return res == INT_MAX ? 0 : res;
+}
+
+// Ford-Fulkerson algorithm
+int maxFlow(const vector<vector<int> > &g, int source, int dest)
+{
+    vector<vector<int> > residual = g; // residual graph for original graph
+    int res = 0, cap = 0;
+    while ((cap = findAugmentingPath(residual, source, dest)) > 0)
+        res += cap;
+    return res;
+}
 
 int main()
 {
@@ -57,5 +97,15 @@ int main()
     #endif
     ////////////////////////////////////////////////////////////////////////////
 
-  
+    int n, m;
+    cin >> n >> m;
+    vector< vector<int> > g(m, vector<int>(m));
+    for (int i = 0; i < n; ++i) {
+        int x, y, capacity;
+        cin >> x >> y >> capacity;
+        --x, --y;
+        g[x][y] += capacity;
+        // g[y][x] += capacity;
+    }
+    cout << maxFlow(g, 0, m - 1) << endl;
 }
